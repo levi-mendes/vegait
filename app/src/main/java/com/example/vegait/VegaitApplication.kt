f256dfc477d24b31b388f78e856f0cbe93780a1c
Levi Mendes <levi.mendes@foursys.com.br>
2023-08-31 22:34:21 -0300

    adicionado injecao de dependencia

--- app/src/main/java/com/example/vegait/VegaitApplication.kt
+++ app/src/main/java/com/example/vegait/VegaitApplication.kt
@@ -1,19 +1,19 @@
 package com.example.vegait
 
 import android.app.Application
-//import com.example.vegait.di.network
-//import org.koin.android.ext.koin.androidContext
-//import org.koin.core.context.startKoin
+import com.example.vegait.di.network
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class VegaitApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
-//        startKoin {
-//            modules(arrayListOf(
-//                network
-//            ))
-//            androidContext(this@VegaitApplication)
-//        }
+        startKoin {
+            modules(arrayListOf(
+                network
+            ))
+            androidContext(this@VegaitApplication)
+        }
     }
 }
